0f4100ab118041e1dd808f13d01273898cfa73a6
Dmitriy Bondarenko <d29bondarenko@gmail.com>
2022-11-02 02:47:55 +0700

    add Header in App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   const { tg, onClickToggle } = useTelegram();
 
   useEffect(() => {
     tg.ready();
-  }, [tg]);
+  }, []);
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onClickToggle}>Toggle</button>
     </div>
   );
